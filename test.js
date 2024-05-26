c33413ea7a96434b3c237fb66e5b33bbcba4f706
mlow81072@proton.me <>
2024-05-27 00:46:47 +1200

    added conditional admin build support

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 require('@dotenvx/dotenvx').config({path:'.env.production'})
 //require('@dotenvx/dotenvx').config()
+    //"build": "cross-env npm run clean && npm run build:server && if [ \"${DEPLOYMENT_TYPE}\" == 'admin' ]; then npm run build:admin; fi",
+//    "build": "cross-env npm run clean && npm run build:server && npm run build:admin",
 
 console.log(process.env)
